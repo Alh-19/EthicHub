b6be885c37540494c9d0bd9aaceaea6639a9b7b1
Erikalinares <erikalinares421@gmail.com>
2023-07-04 12:59:19 +0200

    actualizacion de readme

--- ETHICHUB8/ethichub8/src/index.js
+++ ETHICHUB8/ethichub8/src/index.js
@@ -4,17 +4,16 @@ import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { DataContextProvider, DataContext } from './Data/DataContextProvider'; 
-
 import client from './apolloClient'; // Assuming you have created an Apollo client
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+createRoot (rootElement).render(
   <ApolloProvider client={client}>
-    <Router>
-    <DataContextProvider>
-      <App />
-    </DataContextProvider>
-    </Router>
-  </ApolloProvider>,
+  <DataContextProvider>
+  <App />
+  </DataContextProvider>
+ </ApolloProvider>,
   document.getElementById('root')
 );
 
